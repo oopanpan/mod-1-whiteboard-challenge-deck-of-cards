9d3125ef70a2c6cf18990f534e16ee9ffdc431bf
oopanpan <oopanpan@gmail.com>
2021-01-25 10:08:14 -0800

    commit tree 0f9c732e6e24b9e387666e85e0766e5152224cc0

--- solution.rb
+++ solution.rb
@@ -1,7 +1,45 @@
+require 'pry'
 class Deck
 
+    @@all =[]
+
+    def self.all
+        @@all
+    end
+
+
+    SUIT = ["Hearts", "Clubs", "Diamonds", "Spades"]
+    RANK = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    attr_accessor :cards
+
+    def initialize
+        @cards = []
+        SUIT.each do |suit| 
+            RANK.each do|rank|
+                # binding.pry
+                @cards << Card.new(suit,rank)
+            end
+        end
+        @@all << self
+    end
+
+    def choose_card
+        self.cards.delete(self.cards.sample)
+    end
 end
 
 class Card
 
+    attr_accessor :suit, :rank
+    
+    def initialize(suit, rank)
+        @suit = suit
+        @rank = rank
+    end
+
 end
+
+deck = Deck.new
+# binding.pry
+puts Deck.all.inspect
